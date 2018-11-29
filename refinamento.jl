16803487d4acdb15a9e4bbed38f838f776b08abd
leticiaoliv <43250283+leticiaoliv@users.noreply.github.com>
2018-11-29 11:46:03 -0200

    Update refinamento.jl

--- refinamento.jl
+++ refinamento.jl
@@ -2,6 +2,9 @@ using Plots
 gr(size=(600,600))
 default(fmt=:png)
 
+gr(size=(600,600))
+default(fmt=:png)
+
 function declupivot(A::Matrix; diagtol = 1e-12)
     n = size(A, 2)
     p = collect(1:n)
@@ -38,41 +41,28 @@ function declurefine(A, b)
     c = b[p]
     y = L \ c
     x = U \ y
-    E = zeros(length(b))
+    E = zeros(length(b)*2)
     println(p)
     println(x)
     println(A*x - b)
-    for i = 1:length(b) #ver aqui
-        xi = BigFloat.(x)
-        ri = b - A * xi
-        yi = L \ ri[p]
-        Δi = U \ yi
-        x = xi + Δi
-        ei = BigFloat(norm(x - xi))
-        E[i] = ei
-        println("r$i = $(norm(ri))")
+    for i = 1:length(b)*2 
+        xᵢ = BigFloat.(x)
+        rᵢ = b - A * xᵢ
+        yᵢ = L \ rᵢ[p]
+        Δᵢ = U \ yᵢ
+        x = xᵢ + Δᵢ
+        eᵢ = BigFloat(norm(x - xᵢ))
+        E[i] = eᵢ
+        println("r$i = $(norm(rᵢ))")
         println("x = $x")
-        println("Erro = $ei")
+        println("Erro = $eᵢ")
     end
     return E
 end
 
-#A = [4.0 -1 0 -1; 1 -2 1 0; 0 4 -4 1; 5 0 5 -10]
-#b = [6.0, 8, -7, -40]
-#A = [2.0 3 -1; -3 5 6; 1 1 2]
-#b = [-4.0, 19, 11]
-#A = [1.0 1; 99.4 99.9]
-#b = [1.0, 99.2]
-#A = [2.0 1 7 4 -3 -1 4 4 7 0; 4 2 2 3 -2 0 3 3 4 1; 3 4 4 2 1 -2 2 1 9 -3; 9 3 5 1 0 5 6 -5 -3 4; 2 0 7 0 -5 7 1 0 1 6; 1 9 8 0 3 9 9 0 0 5; 4 1 9 0 4 3 7 -4 1 3; 6 3 1 1 6 8 3 3 0 2; 6 5 0 -7 7 -7 6 2 -6 1; 1 6 3 4 8 3 -5 0 -6 0]
-#b = [86.0, 45, 52.5, 108, 66.5, 90.5, 139, 61, -43.5, 31]
-#A = [4 -1 0 -1 0 0 0 0 0 0; -1 4 -1 0 -1 0 0 0 0 0; 0 -1 4 0 0 -1 0 0 0 0;
-    #-1 0 0 4 -1 0 0 0 0 0; 0 -1 0 -1 4 -1 -1 0 0 0; 0 0 -1 0 -1 4 0 -1 0 0;
-    #0 0 0 0 -1 0 4 -1 0 0; 0 0 0 0 0 -1 -1 4 -1 0; 0 0 0 0 0 0 0 -1 4 -1;
-    #0 0 0 0 0 0 0 0 -1 4]
-#b = [-110, -30, -40, -110, 0, -15, -90, -25, -55, -65]
 E = declurefine(A,b)
 D = E[1:10]
 A=[1:10]
 scatter(A, D, c=:blue, leg=:false, yaxis=:log)
-name = @sprintf("Erro_refinamento4")
+name = @sprintf("Errorefinamento")
 png(name)
